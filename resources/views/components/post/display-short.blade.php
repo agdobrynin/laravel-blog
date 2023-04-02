9c7dd1732dec1cd445ec079b1cffd5e07d2ac98f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-04-02 18:11:53 +0400

    fix: Again fix for badge in updated posts card.

--- resources/views/components/post/display-short.blade.php
+++ resources/views/components/post/display-short.blade.php
@@ -7,11 +7,11 @@
         </div>
         <div class="card-body">
             <p> {{ $shortContent() }} </p>
-            <div class="text-muted fw-lighter position-relative">
+            <div class="text-muted fw-lighter">
                 {{ $post->updated_at->diffForHumans()  }}
 
                 @if($isUpdated())
-                    <span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-primary">
+                    <span class="badge rounded-pill bg-primary">
                         🔔 updated
                         <span class="visually-hidden">was updated</span>
                     </span>
