7b8dd2f2e1d66b5c636d42e6b01cb17a3ffc904b
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-04-02 18:08:50 +0400

    fix: Badge for updated posts.

--- resources/views/components/post/display-short.blade.php
+++ resources/views/components/post/display-short.blade.php
@@ -7,14 +7,15 @@
         </div>
         <div class="card-body">
             <p> {{ $shortContent() }} </p>
-            <div class="text-muted fw-lighter">
-                <small>
-                    {{ $post->updated_at->diffForHumans()  }}
+            <div class="text-muted fw-lighter position-relative">
+                {{ $post->updated_at->diffForHumans()  }}
 
-                    @if($isUpdated())
-                        <span class="badge text-bg-secondary">🔔 updated</span>
-                    @endif
-                </small>
+                @if($isUpdated())
+                    <span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-primary">
+                        🔔 updated
+                        <span class="visually-hidden">was updated</span>
+                    </span>
+                @endif
             </div>
         </div>
         <div class="card-footer text-end">
