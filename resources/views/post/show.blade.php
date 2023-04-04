ca7f338bc72bc44887948ad5b07def88e7734dd8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-04-04 18:47:25 +0400

    fix: Show post with box and shadow.

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,17 +1,20 @@
 <x-app-layout pageTitle="Post: {{$pageTitle}}">
-    <h4>{{ $post->title }}</h4>
-    <div class="text-muted fw-lighter">
-        Created: {{ $post->created_at }} ({{ $post->created_at->diffForHumans() }})
-        @if($post->created_at != $post->updated_at)
-            / last update {{ $post->updated_at->diffForHumans() }}
-        @endif
-    </div>
-    <div class="mt-4 mb-4 border-top pt-4">
-        {{ $post->content }}
+    <div class="border rounded shadow-sm p-3">
+        <h4>{{ $post->title }}</h4>
+        <div class="text-muted fw-lighter">
+            Created: {{ $post->created_at }} ({{ $post->created_at->diffForHumans() }})
+            @if($post->created_at != $post->updated_at)
+                / last update {{ $post->updated_at->diffForHumans() }}
+            @endif
+        </div>
+        <div class="mt-4 mb-4 pt-4">
+            {{ $post->content }}
+        </div>
+        <hr>
+        <x-post.action :$post :showView="false"/>
     </div>
 
-    <x-post.action :$post :showView="false"/>
-
+    <h4 class="mt-4">Comments</h4>
     @forelse($post->comments as $comment)
         <x-comment.item :$comment />
     @empty
