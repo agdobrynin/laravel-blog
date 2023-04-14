4d41a51285ecc652f858f7933e9504b0a869073f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-04-14 19:08:27 +0400

    feat: Add attribute target class constants.

--- app/Attribute/Description.php
+++ app/Attribute/Description.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace App\Attribute;
 
-#[\Attribute]
+#[\Attribute(\Attribute::TARGET_CLASS_CONSTANT)]
 class Description
 {
     public function __construct(public string $description)
