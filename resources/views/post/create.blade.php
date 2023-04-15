e1004505a61dd4ceb5c193b65151bb955a0964c3
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-04-15 18:58:10 +0400

    fix: Form in new post component.

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,10 +1,8 @@
 <x-app-layout pageTitle="{{ __('Новый пост')  }}">
-    <div class="col col-lg-6 mx-auto">
-        <x-post.form
-            actionTitle="{{ __('Создать') }}"
-            route="{{ route('post.store') }}"
-            :post="null"
-            class="shadow-lg p-4"
-        />
-    </div>
+    <x-post.form
+        actionTitle="{{ __('Создать') }}"
+        route="{{ route('post.store') }}"
+        :post="null"
+        class="shadow-lg p-4"
+    />
 </x-app-layout>
