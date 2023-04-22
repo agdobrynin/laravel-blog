f7bdd1baa9e656036fd271891bf0e2d35afa2016
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-04-22 10:16:19 +0400

    feat: Mail notification about new comment with embed image.

--- resources/views/emails/comment/ru/new.blade.php
+++ resources/views/emails/comment/ru/new.blade.php
@@ -6,8 +6,10 @@
     }
     blockquote {
         border-left: 2px solid maroon;
-        padding: 1em 0 1em 2em;
+        padding: 1em 1em 1em 2em;
         margin-left: 1em;
+        box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+        background-color: white;
     }
 
     .avatar {
@@ -15,8 +17,14 @@
         width: 50px;
         height: 50px;
         border-radius: 50%;
-        margin-right: 1em;
+        margin: 0em 1em 1em 0em;
         float: left;
+        border: 1px solid gray;
+        box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+    }
+
+    .clear {
+        clear: both;
     }
 </style>
 <p>Привет {{ $comment->commentable->user->name }}</p>
@@ -30,5 +38,16 @@
     @endif
     написал:</p>
 <blockquote>
+    @if($comment->user?->image)
+        <img class="avatar"
+             alt="{{ __('Аватар пользователя') }}"
+             src="{{ $message->embed($comment->user->image->fullPath()) }}"/>
+    @else
+        <img class="avatar"
+             alt="{{ __('Аватар пользователя') }}"
+             src="{{ $message->embed(resource_path('/images/unicorn-icon-svgrepo-com.svg')) }}"/>
+    @endif
+
     {{ $comment->content }}
+    <div class="clear"></div>
 </blockquote>
