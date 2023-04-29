1d857523283b4a9a2965c5ba6686bf3a845f318a
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-04-29 21:56:01 +0400

    feat: Console command for clear image thumbnails

---
+++ app/Console/Commands/CacheClearImageThumbnail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\StoragePathEnum;
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\FilesystemManager;
+
+class CacheClearImageThumbnail extends Command
+{
+    protected const AVATAR = 'avatar';
+    protected const POST = 'post';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cache:clear-image-thumbnail {--' . self::AVATAR . ' : Clear thumbnails for user avatars} {--' . self::POST . ' : Clear thumbnails for blog posts}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear thumbnails for images (avatars, blog post)';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(FilesystemManager $storage): void
+    {
+        $choices[] = $this->option(self::AVATAR) ? self::AVATAR : null;
+        $choices[] = $this->option(self::POST) ? self::POST : null;
+        $choices = array_filter($choices);
+
+        if (!$choices) {
+            $choices = $this->choice(
+                'Choose thumbnails for clear',
+                [
+                    self::AVATAR => 'Users avatar thumbnails',
+                    self::POST => 'Image blog posts'
+                ],
+                multiple: true
+            );
+        }
+
+        $directories = array_reduce($choices, function ($acc, $choice) {
+            if ($choice === self::AVATAR) {
+                $acc[] = StoragePathEnum::USER_AVATAR_THUMB->value;
+            } elseif ($choice === self::POST) {
+                $acc[] = StoragePathEnum::POST_IMAGE_THUMB->value;
+            }
+
+            return $acc;
+        }, []);
+
+        foreach ($directories as $directory) {
+            $storage->deleteDirectory($directory);
+            $this->info('Delete directory with thumbnails: ' . $directory);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\StoragePathEnum;
 use Illuminate\Database\Seeder;
+use Illuminate\Filesystem\FilesystemManager;
 use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
@@ -11,11 +13,16 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run(FilesystemManager $storage): void
     {
         if ($this->command->confirm('Refresh database ?', true)) {
             $this->command->call('mig:ref');
             $this->command->info('Database was refreshed');
+
+            foreach (StoragePathEnum::cases() as $case) {
+                $storage->deleteDirectory($case->value);
+                $this->command->info('Delete storage directory: '.$case->value);
+            }
         }
 
         $this->call([
