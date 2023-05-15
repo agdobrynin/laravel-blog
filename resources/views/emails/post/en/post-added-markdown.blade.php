05cc069631c21a7e5702070ae19725c65fa06b81
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-05-15 10:18:58 +0400

    fix: Email template for info about new post.

--- resources/views/emails/post/en/post-added-markdown.blade.php
+++ resources/views/emails/post/en/post-added-markdown.blade.php
@@ -2,29 +2,27 @@
 @php use App\Enums\LocaleEnums;use Illuminate\Support\Str; @endphp
 @php URL::defaults(['locale' => LocaleEnums::EN->value]) @endphp
 <x-mail::message>
-    ### Hello {{ $user->name }}!
+### Hello {{ $user->name }}!
 
-    New post was added:
+New post was added:
 
-    <x-mail::button url="{{ route('posts.show', [$post]) }}">
-        {{ $post->title }}
-    </x-mail::button>
+<x-mail::button url="{{ route('posts.show', [$post]) }}">{{ $post->title }}</x-mail::button>
 
-    Author {{ $post->user->name }} wrote:
-    <x-mail::panel>
-        <x-emails.user-avatar :fullOrigPath="$post->user?->image?->fullOrigPath()"/>
+Author {{ $post->user->name }} wrote:
+<x-mail::panel>
+<x-emails.user-avatar :fullOrigPath="$post->user?->image?->fullOrigPath()"/>
 
-        {{ Str::limit($post->content) }}
+{{ Str::limit($post->content) }}
 
 
-        <p style="font-size: 0.75em">
-            Post tags:
-            @foreach($post->tags as $tag)
-                - {{ $tag->name }}
-            @endforeach
-        </p>
-    </x-mail::panel>
+<p style="font-size: 0.75em">
+    Post tags:
+    @foreach($post->tags as $tag)
+        - {{ $tag->name }}
+    @endforeach
+</p>
+</x-mail::panel>
 
-    Sincerely yours,<br>
-    {{ config('app.name') }}
+Sincerely yours,<br>
+{{ config('app.name') }}
 </x-mail::message>
--- resources/views/emails/post/ru/post-added-markdown.blade.php
+++ resources/views/emails/post/ru/post-added-markdown.blade.php
@@ -2,29 +2,27 @@
 @php use App\Enums\LocaleEnums;use Illuminate\Support\Str; @endphp
 @php URL::defaults(['locale' => LocaleEnums::RU->value]) @endphp
 <x-mail::message>
-    ### Привет {{ $user->name }}!
+### Привет {{ $user->name }}!
 
-    На сайт добавлен новый пост:
+На сайт добавлен новый пост:
 
-    <x-mail::button url="{{ route('posts.show', [$post]) }}">
-        {{ $post->title }}
-    </x-mail::button>
+<x-mail::button url="{{ route('posts.show', [$post]) }}">{{ $post->title }}</x-mail::button>
 
-    Автор {{ $post->user->name }} написал:
-    <x-mail::panel>
-        <x-emails.user-avatar :fullOrigPath="$post->user?->image?->fullOrigPath()"/>
+Автор {{ $post->user->name }} написал:
+<x-mail::panel>
+<x-emails.user-avatar :fullOrigPath="$post->user?->image?->fullOrigPath()"/>
 
-        {{ Str::limit($post->content) }}
+{{ Str::limit($post->content) }}
 
 
-        <p style="font-size: 0.75em">
-            Тэги поста:
-            @foreach($post->tags as $tag)
-                - {{ $tag->name }}
-            @endforeach
-        </p>
-    </x-mail::panel>
+<p style="font-size: 0.75em">
+Тэги поста:
+@foreach($post->tags as $tag)
+- {{ $tag->name }}
+@endforeach
+</p>
+</x-mail::panel>
 
-    С уважением Ваш,<br>
-    {{ config('app.name') }}
+С уважением Ваш,<br>
+{{ config('app.name') }}
 </x-mail::message>
