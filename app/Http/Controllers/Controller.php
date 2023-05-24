261ac484a1c1ca3182acfbadf372ff39d909a2b5
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-05-24 12:57:28 +0400

    dev: First init swagger generator.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Attributes\Info;
 
+#[Info(version: "1.0.1", title: "Blog post api")]
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
